b9aaef518e3a79869dcb125cad5ec1e02fd0a375
Wassim Vitols <eywassim@gmail.com>
2015-07-09 20:53:59 +0930

    move templates to /templates

--- html5/entryPoints/public/app.js
+++
@@ -1,9 +0,0 @@
-var App = {
-	"commandBus" : "ws://localhost:8123/commandBus",
-	"dependencies" : {
-		"common" : []
-	},
-	"entryPoints" : {
-		"start" : [ "common" ]
-	}
-};
--- html5/entryPoints/public/app/start.js
+++
@@ -1,5 +0,0 @@
-var App = App || {};
-
-App.start = function() {
-	document.getElementById("content").innerHTML = "app/start.js";
-}
